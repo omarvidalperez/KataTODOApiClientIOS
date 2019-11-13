f3e2212cb6db71851bb0ddbf881fe235b426a168
Omar Vidal Pérez <omarvidalperez1992@gmail.com>
2019-11-13 12:04:41 +0100

    Added Test

--- KataTODOAPIClientTests/TODOAPIClientTests.swift
+++ KataTODOAPIClientTests/TODOAPIClientTests.swift
@@ -30,6 +30,7 @@ class TODOAPIClientTests: XCTestCase {
     fileprivate let apiClient = TODOAPIClient()
     fileprivate let anyTask = TaskDTO(userId: "1", id: "2", title: "Finish this kata", completed: true)
 
+    // Get All Tasks
     func testSendsContentTypeHeader() {
         stub(condition: isMethodGET() &&
             isHost("jsonplaceholder.typicode.com") &&
@@ -78,11 +79,143 @@ class TODOAPIClientTests: XCTestCase {
         }
         expect { try result?.get() }.toEventually(throwError(TODOAPIClientError.networkError))
     }
+    
+    func testEmptyListTask() {
+        stub(condition: isMethodGET() &&
+            isHost("jsonplaceholder.typicode.com") &&
+            isPath("/todos")) { _ in
+                let stubPath = OHPathForFile("getEmptyTasksResponse.json", type(of: self))
+                return fixture(filePath: stubPath!, status: 200, headers: ["Content-Type": "application/json"])
+        }
+
+        var result: Result<[TaskDTO], TODOAPIClientError>?
+        apiClient.getAllTasks { response in
+            result = response
+        }
+        expect { try? result?.get().count }.toEventually(equal(0))
+    }
+    
+    //Get Task by Id
+    func testGetTaskById() {
+        stub(condition: isMethodGET() &&
+            isHost("jsonplaceholder.typicode.com") &&
+            isPath("/todos/1")) { _ in
+                let stubPath = OHPathForFile("getTaskByIdResponse.json", type(of: self))
+                return fixture(filePath: stubPath!, status: 200, headers: ["Content-Type": "application/json"])
+        }
+
+        var result: Result<TaskDTO, TODOAPIClientError>?
+        
+        apiClient.getTaskById("1") { (response) in
+            result = response
+        }
+        
+        expect(result).toEventuallyNot(beNil())
+        assertTaskContainsExpectedValues(try! (result?.get())!)
+    }
+    
+    func testGetTaskByIdServerError() {
+        stub(condition: isMethodGET() &&
+            isHost("jsonplaceholder.typicode.com") &&
+            isPath("/todos/1")) { _ in
+                let serverError = NSError(domain: NSURLErrorDomain,
+                                                code: 500)
+                return OHHTTPStubsResponse(error: serverError)
+        }
+        
+        var result: Result<TaskDTO, TODOAPIClientError>?
+        
+        apiClient.getTaskById("1") { (response) in
+            result = response
+        }
+        
+        expect(result).toEventuallyNot(beNil())
+        expect { try result?.get() }.toEventually(throwError(TODOAPIClientError.networkError))
+    }
+    
+    
+    //Delete Task
+    func testDeleteTaskById() {
+        
+        stub(condition: isMethodDELETE() &&
+            isHost("jsonplaceholder.typicode.com") &&
+            isPath("/todos/1")) { _ in
+                return fixture(filePath: "", status: 200, headers: ["Content-Type": "application/json"])
+        }
+
+        var result: Result<Void, TODOAPIClientError>?
+        apiClient.deleteTaskById("1") { response in
+            result = response
+        }
+
+        expect(result).toEventuallyNot(beNil())
+        expect { try result?.get() }.toNot(throwError())
+    }
+
+    func testDeleteTaskByIdErrorItemNotFound() {
+        stub(condition: isMethodDELETE() &&
+            isHost("jsonplaceholder.typicode.com") &&
+            isPath("/todos/1")) { _ in
+                return fixture(filePath: "", status: 404, headers: ["Content-Type": "application/json"])
+        }
+
+        var result: Result<Void, TODOAPIClientError>?
+        apiClient.deleteTaskById("1") { response in
+            result = response
+        }
+
+        expect { try result?.get() }.toEventually(throwError(TODOAPIClientError.itemNotFound))
+    }
+
+    func testDeleteTaskByIdErrorNotConnection() {
+        stub(condition: isMethodDELETE() &&
+            isHost("jsonplaceholder.typicode.com") &&
+            isPath("/todos/1")) { _ in
+                let notConnectedError = NSError(domain: NSURLErrorDomain,
+                                                code: URLError.notConnectedToInternet.rawValue)
+                return OHHTTPStubsResponse(error: notConnectedError)
+        }
+
+        var result: Result<Void, TODOAPIClientError>?
+        apiClient.deleteTaskById("1") { response in
+            result = response
+        }
+
+        expect { try result?.get() }.toEventually(throwError(TODOAPIClientError.networkError))
+    }
+
+    func testDeleteTaskByIdErrorUnknow() {
+        stub(condition: isMethodDELETE() &&
+            isHost("jsonplaceholder.typicode.com") &&
+            isPath("/todos/1")) { _ in
+                return fixture(filePath: "", status: 444, headers: ["Content-Type": "application/json"])
+        }
+
+        var result: Result<Void, TODOAPIClientError>?
+        apiClient.deleteTaskById("1") { response in
+            result = response
+        }
+
+        expect { try result?.get() }.toEventually(throwError(TODOAPIClientError.unknownError(code: 444)))
+    }
+    
+    //Update Task
+    
+    
+    
+    
 
     fileprivate func assertTaskContainsExpectedValues(_ task: TaskDTO) {
         expect(task.id).to(equal("1"))
         expect(task.userId).to(equal("1"))
         expect(task.title).to(equal("delectus aut autem"))
         expect(task.completed).to(beFalse())
+    }
+    
+    fileprivate func assertUpdatedTaskContainsExpectedValues(_ task: TaskDTO) {
+        expect(task.id).to(equal("2"))
+        expect(task.userId).to(equal("1"))
+        expect(task.title).to(equal("Finish this kata"))
+        expect(task.completed).to(beTrue())
     }
 }
